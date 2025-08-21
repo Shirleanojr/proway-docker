39bcaf5b54235d24442e845dd011695dc59cf3d1
Shirleano Dacio Junior <shirleanojr@gmail.com>
2025-08-21 19:52:42 -0300

    Create deploy-debian.sh

---
+++ deploy-debian.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+usage() {
+  cat <<EOF
+Set env !!! ONLY ROOT !!!!!
+
+USAGE:  ./deploy.sh --REPO_DIR [REPO_DIR] --BRANCH [BRANCH] --LOG_FILE [LOG_FILE]
+
+ARGS:
+  --REPO_DIR        Git local repository
+  --BRANCH          Branch to track (e.g: main).
+  --LOG_FILE        PATH log file
+
+NOTES:
+  - Your app repo must have a Dockerfile at its root.
+  - If the repo lacks docker-compose.yml, a minimal one will be installed.
+EOF
+}
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --REPO_DIR) REPO_DIR="$2"; shift 2;;
+    --BRANCH) BRANCH="$2" ; shift 2;;
+    --LOG_FILE) LOG_FILE="$2" ; shift 2;;
+    --help|-h) usage; exit 0 ;;
+    *) echo "Unknow arg: $1" >&2; usage; exit 1;;
+  esac
+done
+
+
+echo "Setting variables"
+WORK_DIR="/opt/repos/"
+SCRIPT_PATH="/opt/deploy"
+APP_ENV="$SCRIPT_PATH/app.env"
+
+echo "Creating default work folders"
+mkdir -p $WORK_DIR
+mkdir -p $SCRIPT_PATH
+
+echo "Creating app environment variables"
+cat > $APP_ENV <<EOF
+REPO_DIR=$REPO_DIR
+BRANCH=$BRANCH
+LOG_FILE=$LOG_FILE/app.log
+EOF
+
+source $APP_ENV
+
+
+echo "Updating package definitions"
+apt update 
+
+echo "Installing Dependencies"
+apt install curl git -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+echo "Cloning App repository"
+git clone -b "$BRANCH" "$REPO_DIR" "$WORK_DIR"
+
+WORK_DIR="$WORK_DIR/pizzaria-app/"
+echo "WORK_DIR=/opt/repos/pizzaria-app" >> $APP_ENV
+source $APP_ENV
+
+
+
+echo "Start App"
+docker compose -f "$WORK_DIR"/docker-compose.yml up --build -d
+
+
+echo "Set crontab for update local repo every new commit in main branch on github repo"
+cat > $SCRIPT_PATH/cron-get-update.sh <<'EOF'
+#!/bin/bash
+source /opt/deploy/app.env
+
+
+git -C "$WORK_DIR" fetch
+remote_sha="$(git -C "$WORK_DIR" rev-parse "origin/$BRANCH")"
+local_sha="$(git -C "$WORK_DIR" rev-parse "$BRANCH")"
+
+
+if [[ "$remote_sha" != "$local_sha" ]]; then
+  echo "$(date '+%F %T') [DEBUG] new change detected $BRANCH @ $remote_sha" >> "$LOG_FILE"
+  echo "$(date '+%F %T') [DEBUG] Stop App" >> "$LOG_FILE"
+  docker compose -f "$WORK_DIR"/docker-compose.yml down
+  
+  echo "$(date '+%F %T') [DEBUG] Pull branch $BRANCH" >> "$LOG_FILE" 
+  git pull origin $BRANCH
+
+  echo "$(date '+%F %T') [DEBUG] Start up" >> "$LOG_FILE" 
+  docker compose -f "$WORK_DIR"/docker-compose.yml up --build -d
+  exit 0
+fi
+EOF
+
+SCRIPT_PATH="$SCRIPT_PATH/cron-get-update.sh"
+chmod +x $SCRIPT_PATH
+
+CRON_JOB="*/5 * * * * $SCRIPT_PATH"
+(crontab -l 2>/dev/null | grep -Fv "$SCRIPT_PATH"; echo "$CRON_JOB") | crontab -
